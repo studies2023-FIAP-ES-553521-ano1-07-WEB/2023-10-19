de3e7d4224150f21a13b3bc82c13e0ab70e2355a
Rafafaaa <cristofalirafael@gmail.com>
2023-10-19 20:52:16 -0300

    update

--- forca.js
+++ forca.js
@@ -1,3 +1,25 @@
 const prompt = require('prompt-sync')();
 
-const name = prompt('Qual é seu nome?\n');
+const users = [];
+
+function cadatrarUsuarios(nome, idade, profissao) {
+    const user = {
+        nome,
+        idade,
+        profissao
+    };
+
+    users.push(user);
+}
+
+for (let i = 0; i < 10; i++){
+    const nome = prompt('Digite seu nome: ');
+    const idade = prompt('Digite sua idade: ');
+    const profissao = prompt('Digite sua profissão: ');
+
+    cadatrarUsuarios(nome, idade, profissao);
+}
+
+for (let i = 0; i < users.length; i++) {
+    console.log(users[i].nome);
+}
